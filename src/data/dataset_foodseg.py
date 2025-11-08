81ea0c7da1b7989310ca7c510832534b4b40743a
Yanqian WANG <scyyw13@exmail.nottingham.ac.uk>
2025-11-07 22:05:16 -0500

    add basic framework for unet

--- src/data/dataset_foodseg.py
+++ src/data/dataset_foodseg.py
@@ -4,13 +4,16 @@ from torch.utils.data import Dataset
 import numpy as np
 from PIL import Image
 import torch
+from torchvision import transforms
+import torch.nn.functional as F
 
 # src/data/dataset_foodseg.py
 class FoodSegDataset(Dataset):
-    def __init__(self, split="train", transform=None,
+    def __init__(self, split="train", img_transform=None, mask_transform=None,
                  compute_reflect=False, reflect_threshold=220):
         self.dataset = load_dataset("EduardoPacheco/FoodSeg103")[split]
-        self.transform = transform
+        self.img_transform = img_transform
+        self.mask_transform = mask_transform
         self.compute_reflect = compute_reflect
         self.reflect_threshold = reflect_threshold
 
@@ -19,15 +22,31 @@ class FoodSegDataset(Dataset):
 
     def __getitem__(self, idx):
         image = self.dataset[idx]["image"].convert("RGB")
-        mask = np.array(self.dataset[idx]["label"], dtype=np.int64)
-        sample = {"image": image, "mask": mask}
+        # # Convert mask to NumPy, using int32 to avoid PIL errors
+        mask = np.array(self.dataset[idx]["label"], dtype=np.int32)
+        mask = torch.from_numpy(mask).long()  # (H, W)
+        # sample = {"image": image, "mask": mask}
 
-        # 可选：计算高亮比例
+        # Optional: Calculate highlight ratio
         if self.compute_reflect:
             gray = np.array(image.convert("L"))
             reflect_ratio = (gray > self.reflect_threshold).sum() / gray.size
-            sample["reflect_ratio"] = reflect_ratio
+            # return reflect_ratio
+            # sample["reflect_ratio"] = reflect_ratio
 
-        if self.transform:
-            sample = self.transform(sample)
-        return sample
+        if self.img_transform:
+            image = self.img_transform(image)
+        # mask transform (only support resize for now)
+        if self.mask_transform:
+            for t in self.mask_transform.transforms:
+                if isinstance(t, transforms.Resize):
+                    size = t.size
+                    if isinstance(size, int):
+                        size = (size, size)
+                    mask = mask.unsqueeze(0).float()  # (1,H,W)
+                    mask = F.interpolate(mask.unsqueeze(0), size=size, mode="nearest")  # (1,1,H,W)
+                    mask = mask.squeeze(0).squeeze(0).long()  # -> (H,W)
+
+
+
+        return image, mask
